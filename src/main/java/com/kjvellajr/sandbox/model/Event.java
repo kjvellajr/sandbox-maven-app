9c3b8c2a92cb8df9d335cc1fe02da04f4a3ced44
Ken Vella <kjvellajr@gmail.com>
2015-04-03 10:12:14 -0400

    switching to google endpoints api

--- src/main/java/com/kjvellajr/sandbox/model/Event.java
+++ src/main/java/com/kjvellajr/sandbox/model/Event.java
@@ -4,4 +4,17 @@ public class Event {
 	
 	private Integer id;
 	private String displayName;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer pId) {
+		id = pId;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String pDisplayName) {
+		displayName = pDisplayName;
+	}
 }
