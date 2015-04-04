d385420c9c73af92dd5765c96c7c004736737745
Ken Vella <kjvellajr@gmail.com>
2015-04-04 14:16:29 -0400

    reverting to previous HEAD

---
+++ src/main/java/com/kjvellajr/sandbox/events/EventsAPI.java
@@ -0,0 +1,39 @@
+package com.kjvellajr.sandbox.events;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.kjvellajr.sandbox.model.Event;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.inject.Named;
+
+/**
+ * Resource to test RESTful api.
+ * @author Ken Vella
+ */
+@Api(name = "eventsApi", version = "v1", namespace = @ApiNamespace(
+			ownerDomain = "sandbox.kjvellajr.com",
+			ownerName = "sandbox.kjvellajr.com",
+			packagePath = ""))
+public class EventsAPI {
+	public class Events {
+		private Collection<Event> events;
+		public Events(Collection<Event> pEvents) {
+			events = pEvents;
+		}
+		public Collection<Event> getEvents() {
+			return events;
+		}
+		public void setEvents(Collection<Event> pEvents) {
+			events = pEvents;
+		}
+	}
+	@ApiMethod(name = "getEvents")
+	public Events getEvents(@Named("name") String pName) {
+		final Collection<Event> events = new ArrayList<Event>();
+		events.add(new Event());
+		return new Events(events);
+	}
+}
+
---
+++ src/main/java/com/kjvellajr/sandbox/model/Event.java
@@ -0,0 +1,20 @@
+package com.kjvellajr.sandbox.model;
+
+public class Event {
+	
+	private Integer id;
+	private String displayName;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer pId) {
+		id = pId;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String pDisplayName) {
+		displayName = pDisplayName;
+	}
+}
