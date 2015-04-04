790482abed88630355184b1aeb4996eb004714cf
Ken Vella <kjvellajr@gmail.com>
2015-04-04 16:09:38 -0400

    adding initial jdo framework

--- src/main/java/com/kjvellajr/sandbox/event/EventAPI.java
+++ src/main/java/com/kjvellajr/sandbox/event/EventAPI.java
@@ -3,10 +3,14 @@ package com.kjvellajr.sandbox.event;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.kjvellajr.sandbox.jdo.PMF;
 import com.kjvellajr.sandbox.model.Event;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.inject.Named;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
 
 /**
  * Resource to test RESTful api.
@@ -29,10 +33,27 @@ public class EventAPI {
 			events = pEvents;
 		}
 	}
+	@ApiMethod(name = "addEvent")
+	public Event addEvent(@Named("description") String pDescription) {
+		final Event e = new Event();
+		// todo: create event
+		final PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			// todo: persist event			
+		} finally {
+			pm.close();
+		}
+		return e;
+	}
 	@ApiMethod(name = "getEvents")
-	public Events getEvents(@Named("name") String pName) {
+	public Events getEvents() {
 		final Collection<Event> events = new ArrayList<Event>();
-		events.add(new Event());
+		final PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			//todo: read
+		} finally {
+			pm.close();
+		}
 		return new Events(events);
 	}
 }
